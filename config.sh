ff79c8039684530812061178c8e1f5161b68648d
l1f21bsse0467 <l1f21bsse0467@ucp.edu.pk>
2025-01-26 14:47:08 +0500

    Initial commit

---
+++ config.sh
@@ -0,0 +1,7 @@
+git branch -m develop
+git config --local user.name "l1f21bsse0467"
+git config --local user.email "l1f21bsse0467@ucp.edu.pk"
+mkdir ./styles ./src
+touch ./styles/index.css ./src/index.html
+npm init -y 
+git config --local core.editor notepad
